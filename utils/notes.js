3a68d9e6311fb0ce35e01ca0f7ab2aed5cefe56a
MelindaKleszken <melindakleszken1021@gmail.com>
2021-01-25 14:44:33 +0000

    listNotes function added

--- utils/notes.js
+++ utils/notes.js
@@ -1,15 +1,8 @@
 const fs = require("fs");
 
-const addNote = myNote => {
-    const allNotes = loadNotes();
-    allNotes.push({reminder: myNote});
-
-    saveNotes(allNotes);
-}
-
 const loadNotes = () => {
     try {
-        const dataBuffer = fs.readyFyleSync("src/notes.json");
+        const dataBuffer = fs.readFileSync("src/notes.json");
         const notesJson = dataBuffer.toString();
         return JSON.parse(notesJson);
     } catch (error) {
@@ -17,19 +10,25 @@ const loadNotes = () => {
     }
 };
 
-const saveNotes = (allNotes) => {
+const addNote = myNote => {
+    const allNotes = loadNotes();
+    allNotes.push({reminder: myNote});
+    saveNotes(allNotes);
+};
+
+const saveNotes = allNotes => {
     const notesJson = JSON.stringify(allNotes);
     fs.writeFileSync("src/notes.json", notesJson);
 };
 
 const listNotes = () => {
     const allNotes = loadNotes();
-    allNotes.map((note, index ) => {
+    allNotes.map((note, index) => {
         console.log(`${index + 1}. ${note.reminder}`);
     });
 };
 
 module.exports = {
-    addNote, 
+    addNote,
     listNotes,
 };
